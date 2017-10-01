b37f43d9373d937ce80607445079a61410b98861
Bruno Rocha <mail.brunorocha@gmai.com>
2017-10-01 09:55:39 -0300

    Implementação dos models, controllers e rotas

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var snaps = require('./routes/snaps');
+var media = require('./routes/media');
 
 var app = express();
 
@@ -17,13 +19,15 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/snaps', snaps);
+app.use('/media', media);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +46,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+var port = process.env.PORT || 8080;
+app.listen(port);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var user_controller = require('../controllers/userController.js');
+
+
+// GET users listing.
+router.get('/', user_controller.listAll);
+
+// GET users listing.
+router.get('/:id', user_controller.userById);
+
+// POST user add
+router.post('/', user_controller.add);
+
+// POST user auth
+router.post('/auth', user_controller.auth);
 
 module.exports = router;
