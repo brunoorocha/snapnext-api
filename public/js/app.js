a12a532bbc6031da0caba0da744a7ebed9e2566f
Bruno Rocha <mail.brunorocha@gmai.com>
2017-10-06 09:06:20 -0300

    Menu do botão de configurações

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function() {
-
     var snapnextApiUrl = "https://pacific-taiga-97807.herokuapp.com";
 
     $("#login-form").submit(function(evt) {
@@ -19,6 +18,7 @@ $(document).ready(function() {
         $("#entrar-btn").attr('value', 'autenticando...');
 
         $.post(snapnextApiUrl + "/users/auth", dataSerialized, function(res) {
+            sessionStorage.setItem('userID', res._id);
             $(location).attr('href', './map.html');
         }, "json")
             .fail(function(err) {
@@ -46,5 +46,67 @@ $(document).ready(function() {
                 }
             });
 
+    });
+
+
+    $("#signup-form").submit(function(evt) {
+        evt.preventDefault();
+
+        var usernameField = $('#new-username');
+        var passwordField = $('#new-password');
+        var passwordConfirmField = $('#new-password-confirm');
+
+        if(passwordField.val() != passwordConfirmField.val()) {
+            $('#password-confirm-input').addClass("error-field");
+            return;
+        }
+
+        if(usernameField.val() == '') {
+            $('#username-input').addClass("error-field");
+            return;
+        }
+
+        if(passwordField.val() == '') {
+            $('#password-input').addClass("error-field");
+            return;
+        }
+
+        var dataSerialized = {
+            username: usernameField.val(),
+            password: passwordField.val()
+        }
+
+        $("#entrar-btn").focus().addClass('loading');
+        $("#entrar-btn").attr('disabled');
+        $("#entrar-btn").attr('value', 'verificando...');
+
+        $.post(snapnextApiUrl + "/users/", dataSerialized, function(res) {
+            sessionStorage.setItem('userID', res._id);
+            $(location).attr('href', './map.html');
+        }, "json")
+            .fail(function(err) {
+
+            });
+    });
+
+    $('#new-password-confirm').focus(function() {
+        $('#password-confirm-input').removeClass('error-field');
+    });
+
+    $('#new-username').focus(function() {
+        $('#username-input').removeClass('error-field');
+    });
+
+    $('#new-password').focus(function() {
+        $('#password-input').removeClass('error-field');
+    });
+
+    $('#config-btn').on("click", function() {
+        $('.config-modal').toggleClass('config-modal-actived');
+    });
+
+    var userID = sessionStorage.getItem('userID');
+    $.get(snapnextApiUrl + "/users/" + userID, function(user) {
+        $('.config-modal-username').html(user[0].username);
     });
 });
